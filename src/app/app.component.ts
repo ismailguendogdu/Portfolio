246bffd7a9e8cf225dbadb74ba657f1cbbd8a63b
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-08-06 13:15:03 +0200

    added about me section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeroComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
