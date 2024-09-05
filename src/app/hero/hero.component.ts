2d0d3a914a88762ea34ffacedc5801bc8a23cecf
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-09-05 17:08:06 +0200

    some adds

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../service/translation.service';
 
@@ -11,13 +11,32 @@ import { TranslationService } from '../service/translation.service';
 })
 export class HeroComponent {
   translate = inject(TranslationService);
-  toggleUrl = `../../assets/img/${localStorage.getItem('lang')??'en'}.png`;
-  
+  toggleUrl = `../../assets/img/${localStorage.getItem('lang') ?? 'en'}.png`;
+  isNavbarOpen = false;
+
   selectLanguage() {
     const lang = this.translate.getSelectedLanguage();
-    const newLang = lang === 'en' ? 'de' :'en';
-    this.toggleUrl=`../../assets/img/${newLang}.png`;
+    const newLang = lang === 'en' ? 'de' : 'en';
+    this.toggleUrl = `../../assets/img/${newLang}.png`;
     this.translate.switchLanguage(newLang);
   }
-    
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeNavbarOnClickOutside(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    const navbar = document.getElementById('navbarNav');
+    const toggler = document.querySelector('.navbar-toggler');
+
+    if (navbar && toggler && !navbar.contains(targetElement) && !toggler.contains(targetElement)) {
+      this.isNavbarOpen = false;
+      const navbarCollapse = document.getElementById('navbarNav');
+      if (navbarCollapse) {
+        navbarCollapse.classList.remove('show');
+      }
+    }
+  }
 }
