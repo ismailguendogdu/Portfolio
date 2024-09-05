00dcecd333d76d1ea01d8567bbc1179412a55ba6
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-09-05 15:50:09 +0200

    animation skill icons

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,28 +1,42 @@
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Image } from './../types/image';
-import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { Image } from './../types/image';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule,TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
   images: Array<Image> = [
-     {name:'HTML',url:'./assets/img/html.png'},
-     {name:'CSS',url:'./assets/img/css.png'},
-     {name:'Javascript',url:'./assets/img/javascript.png'},
-     {name:'Material Design',url:'./assets/img/materialdesign.png'},
-     {name:'TypeScript',url:'./assets/img/typescript.png'},
-     {name:'Angular',url:'./assets/img/angular.png'},
-     {name:'Firebase',url:'./assets/img/firebase.png'},
-     {name:'GIT',url:'./assets/img/git.png'},
-     {name:'Rest API',url:'./assets/img/restapi.png'},
-     {name:'Scrum',url:'./assets/img/scrum.png'},
-     {name:'Growth mindset',url:'./assets/img/growthmindset.png'}
+    {name:'HTML',url:'./assets/img/html.png'},
+    {name:'CSS',url:'./assets/img/css.png'},
+    {name:'Javascript',url:'./assets/img/javascript.png'},
+    {name:'Material Design',url:'./assets/img/materialdesign.png'},
+    {name:'TypeScript',url:'./assets/img/typescript.png'},
+    {name:'Angular',url:'./assets/img/angular.png'},
+    {name:'Firebase',url:'./assets/img/firebase.png'},
+    {name:'GIT',url:'./assets/img/git.png'},
+    {name:'Rest API',url:'./assets/img/restapi.png'},
+    {name:'Scrum',url:'./assets/img/scrum.png'},
+    {name:'Growth mindset',url:'./assets/img/growthmindset.png'}
   ];
 
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    });
+
+    const icons = this.el.nativeElement.querySelectorAll('.icon');
+    icons.forEach((icon: Element) => observer.observe(icon));
+  }
 }
